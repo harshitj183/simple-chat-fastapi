143e4cb682e066fbe058d15fec2dcc000e24551a
mikhailofff <asdgrghd@gmail.com>
2026-01-14 17:33:08 +0400

    test: added tests for send message endpoint

---
+++ backend/tests/test_api/test_send_message.py
@@ -0,0 +1,90 @@
+import pytest
+import httpx
+from datetime import datetime
+
+from ..conftest import create_expired_token
+
+from src.schemas.config import settings
+
+
+@pytest.mark.order(after="tests/test_api/test_token.py::test_token_with_unauthorized_username")
+@pytest.mark.asyncio
+async def test_send_message(async_client: httpx.AsyncClient):
+    access_token = async_client.cookies.get("access_token")
+    message_request = {
+        "content": "Hello world!",
+        "created_at": datetime.now().isoformat(),
+        "updated_at": None,
+        "created_by": "testname"
+    }
+    response = await async_client.post(
+        "/api/send-message",
+        json=message_request,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == 1
+
+
+@pytest.mark.order(after="test_send_message")
+@pytest.mark.asyncio
+async def test_send_message_as_unauthorized(async_client: httpx.AsyncClient):
+    message_request = {
+        "content": "Hello world!",
+        "created_at": datetime.now().isoformat(),
+        "updated_at": None,
+        "created_by": "testname"
+    }
+    response = await async_client.post(
+        "/api/send-message",
+        json=message_request
+    )
+
+    assert response.status_code == 401
+
+
+@pytest.mark.order(after="test_send_message_as_unauthorized")
+@pytest.mark.asyncio
+async def test_send_message_with_invalid_token(async_client: httpx.AsyncClient):
+    access_token = "invalid_token"
+    message_request = {
+        "content": "Hello world!",
+        "created_at": datetime.now().isoformat(),
+        "updated_at": None,
+        "created_by": "testname"
+    }
+    response = await async_client.post(
+        "/api/send-message",
+        json=message_request,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+    assert response.status_code == 401
+
+
+@pytest.mark.order(after="test_send_message_with_invalid_token")
+@pytest.mark.asyncio
+async def test_send_message_with_expired_token(async_client: httpx.AsyncClient):
+    expired_token = await create_expired_token({"sub": "testname"}, settings.SECRET_KEY)
+
+    message_request = {
+        "content": "Hello world!",
+        "created_at": datetime.now().isoformat(),
+        "updated_at": None,
+        "created_by": "testname"
+    }
+    response = await async_client.post(
+        "/api/send-message",
+        json=message_request,
+        headers={
+            "Authorization": f"Bearer {expired_token}"
+        }
+    )
+
+    assert response.status_code == 401
